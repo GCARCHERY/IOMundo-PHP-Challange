36a0eb19fcb30e1203ad632c7aca222163d1f7a2
Overseer <tudor_cg@yahoo.com>
2023-03-05 15:21:41 +0200

    add .env support

--- eloquent/bootstrap.php
+++ eloquent/bootstrap.php
@@ -3,14 +3,15 @@
 require "vendor/autoload.php";
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+$env = parse_ini_file('.env');
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    "driver" => "mysql",
-    "host" => "127.0.0.1",
-    "database" => "iomundo",
-    "username" => "root",
-    "password" => "root1234"
+    "driver" => $env['DB_DRIVER'],
+    "host" => $env['DB_HOST'],
+    "database" => $env['DB_NAME'],
+    "username" => $env['DB_USERNAME'],
+    "password" => $env['DB_PASSWORD'],
 ]);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
